5175ede149f25dbd698588ad97f1fb09dbecae6c
zachibs <zachibsn@gmail.com>
2022-12-26 15:19:25 +0200

    small change

--- SAT0_OBC/SatImageTaking/start_service.py
+++ SAT0_OBC/SatImageTaking/start_service.py
@@ -2,7 +2,7 @@
     @file start_service.py
     @brief Main file for SATLLA0 OBC picture taking mission.
 
-    Copyright (C) 2023 @authors Zachi Ben-Shtirit, Assaf Hiya
+    Copyright (C) 2023 @authors Zachi Ben-Shtirit, Assaf Hiya.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
