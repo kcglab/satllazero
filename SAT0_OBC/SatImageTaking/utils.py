b26cc52cac6ad0b281288fce6107daefab9b2cd8
zachibs <zachibsn@gmail.com>
2022-12-26 15:15:54 +0200

    picture-taking: added license to the files

--- SAT0_OBC/SatImageTaking/utils.py
+++ SAT0_OBC/SatImageTaking/utils.py
@@ -1,3 +1,22 @@
+'''
+    @file utils.py
+    @brief A file for the utility functions used in the mission.
+
+    Copyright (C) 2023 @authors Zachi Ben-Shtirit, Assaf Hiya
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ '''
 from cv2 import imwrite as cv2_imwrite
 from cv2 import imread as cv2_imread
 from cv2 import resize as cv2_resize
